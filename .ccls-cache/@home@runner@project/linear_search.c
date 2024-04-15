fbabfdc9748ee819a985d30061e2d53000a42596
T. Cagil Oral <119301847+oralc@users.noreply.github.com>
2024-04-15 17:04:58 +0000

    string compare

--- .ccls-cache/@home@runner@project/linear_search.c
+++ .ccls-cache/@home@runner@project/linear_search.c
@@ -1,18 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+//strcmp > for string companre
 
-
-// linear search program
+// string linear search program
 int main(void){
-    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
-
-    int n = get_int("Number: ");
-
-    for (int i=0; i < 7 ; i++)
+    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
+    string s = get_string("String: ");
+    for(int i = 0; i < 6; i++)
     {
-        if (numbers[i] == n){
+        if (strcmp(strings[i], s) == 0) // documentation
+        {
             printf("Found \n");
-            return 0; //indicates success
+            return 0;
         }
     }
     printf("Not Found \n");
