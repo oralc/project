49a15c8c9f0a5072ca4cb0e51192edd937552887
T. Cagil Oral <119301847+oralc@users.noreply.github.com>
2024-04-17 05:29:35 +0000

    witouth using cs50.h for strings

--- adresses.c
+++ adresses.c
@@ -1,7 +1,8 @@
-#include <cs50.h>
+//#include <cs50.h>
 #include <stdio.h>
+typedef char *string;
 
-typedef uint8_t BYTE;
+//typedef uint8_t BYTE;
 
 int main(void)
 {
@@ -15,22 +16,5 @@ int main(void)
     char *st = "Hello" ;
     printf("%s \n", st);
 
-  #include <cs50.h>
-  #include <stdio.h>
-
-  int main(void)
-  {
-      string s = "HI!";
-      printf("%p\n",s);
-      printf("%p\n",&s[0]);
-      printf("%p\n",&s[1]);
-      printf("%p\n",&s[2]);
-      printf("%p\n",&s); // address of pointer
-
-      char *st = "Hello" ;
-      printf("%s \n", st);
-
-
-  }
 
 }
